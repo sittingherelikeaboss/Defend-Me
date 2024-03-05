52e79fc0540dd46f6c9a0b8d987956db992cc1b1
Jastine Goyena <jastineg@gmail.com>
2024-03-04 21:59:07 -0500

    added not found component for wildcard routing

--- react-client/src/Components/NoFoundComponent/NoFoundComponent.jsx
+++ react-client/src/Components/NoFoundComponent/NoFoundComponent.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
+import "./NoFoundComponent.css";
 
 export const NoFoundComponent = () => {
   return (
     <>
-      <h1>The page you are looking for is not found!</h1>
+      <div className="sorry-wrapper">
+        <h1>Sorry!</h1>
+        <h2>The page you are looking for is not found!</h2>
+      </div>
     </>
   );
 };
